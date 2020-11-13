ef0db27e91ce08f5d287b0f15a32b40df382b622
Moxiney <443928841@qq.com>
2020-11-13 11:09:02 +0800

    cmake config

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout<<"Hello World"<<endl;
+  cout << "Hello World" << endl;
+
+  vector<int> a = {1, 2, 3};
+
+  for (auto i : a) {
+    cout << i << endl;
+  }
 }
